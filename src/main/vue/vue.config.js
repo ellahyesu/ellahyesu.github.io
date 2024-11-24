a86e35bbab52edc015f2c3db1f601ee182309729
Hyesu Kim <hyesukim@kakao.com>
2024-11-24 21:12:21 +0900

    Update vue.config.js
    1. vue.config.js에서 publicPath와 outputDir 설정.
    2. Vue 앱 빌드 후 GitHub Pages에 /docs 디렉토리로 배포.

--- src/main/vue/vue.config.js
+++ src/main/vue/vue.config.js
@@ -1,13 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: '../resources/static', // Build Directory
-    devServer: {
-        proxy: {
-          '/api': {
-              target: 'http://localhost:8080', // Spring Boot 백엔드 URL
-              changeOrigin: true
-          }
+  // outputDir: '../resources/static', // Build Directory
+  publicPath: './', // 상대 경로 설정 (GitHub Pages 호환)
+  outputDir: 'docs', // 빌드된 파일을 /docs 디렉토리에 저장
+  devServer: {
+      proxy: {
+        '/api': {
+            target: 'http://localhost:8080', // Spring Boot 백엔드 URL
+            changeOrigin: true
         }
-    }
+      }
+  }
 })
